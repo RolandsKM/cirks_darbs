c73e2f5eb549c6e209094936000987ce03066a09
rolands <rolandsmakarovs264@gmail.com>
2025-01-08 11:21:15 +0200

    LOW TAPER FADE

---
+++ Assets/Script/FadeScript.cs
@@ -0,0 +1,35 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+    Image img;
+    Color tempColor;
+    void Start()
+    {
+        img = GetComponent<Image>();
+        tempColor = img.color;
+        tempColor.a = 1f;
+        StartCoroutine(FadeOut(0.15f));
+    }
+    IEnumerator FadeOut(float seconds)
+    {
+        for(float a = 1f; a>=-0; a -= 0.05f)
+        {
+            tempColor = img.color;
+            tempColor.a = a;
+            img.color = tempColor;
+            yield return new WaitForSeconds(seconds);
+            
+        }
+        img.raycastTarget = false;
+    }
+    
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# cirks_darbs
+# Cirks
+Unity 2.5D circus poject
+
+### Todo list:
+- [ ] Create main menue
+- [x] Add animations to dice and clouds
+- [x] Change cursor
+- [ ] Add Sound
+- [x] Add settings
+- [ ] Add character selection screen
+- [ ] Add throwable dice
+- [ ] Add leaderboard
+- [ ] Create Circus game
